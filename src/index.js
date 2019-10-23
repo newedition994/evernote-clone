260d10130560bfb6132a07a9c7ce9243eb9b388b
Breyanna Vass <breyannavass@gmail.com>
2019-10-23 12:07:33 -0400

    hiding firebase Key info

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require("firebase");
+require("firebase/firestore");
+
+const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+const DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+const URL = process.env.REACT_APP_FIREBASE_DATABASE_URL;
+const PROJECT = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+const STORAGE = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+const MESSAGING = process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID;
+const APP_ID = process.env.REACT_APP_FIREBASE_APP_ID;
+const MEASUREMENT = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID;
+
+firebase.initializeApp({
+  apiKey: API_KEY,
+  authDomain: DOMAIN,
+  databaseURL: URL,
+  projectId: PROJECT,
+  storageBucket: STORAGE,
+  messagingSenderId: MESSAGING,
+  appId: APP_ID,
+  measurementId: MEASUREMENT
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
