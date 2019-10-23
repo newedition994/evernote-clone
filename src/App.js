01274d356804b09714ac50b7db8809469ed37a15
Breyanna Vass <breyannavass@gmail.com>
2019-10-23 14:31:36 -0400

    some missing elements from application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
-
-import "./App.css";
-
 import SidebarComponent from "./sidebar/sidebar";
 import EditorComponent from "./editor/editor";
+import "./App.css";
 
 const firebase = require("firebase");
 
@@ -15,6 +13,28 @@ class App extends React.Component {
       selectedNote: null,
       notes: null
     };
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <SidebarComponent
+          selectedNoteIndex={this.state.selectedNoteIndex}
+          notes={this.state.notes}
+          deleteNote={this.deleteNote}
+          selectNote={this.selectNote}
+          newNote={this.newNote}
+        ></SidebarComponent>
+        {this.state.selectedNote ? (
+          <EditorComponent
+            selectedNote={this.state.selectedNote}
+            selectedNoteIndex={this.state.selectedNoteIndex}
+            notes={this.state.notes}
+            noteUpdate={this.noteUpdate}
+          ></EditorComponent>
+        ) : null}
+      </div>
+    );
   }
 
   componentDidMount = () => {
@@ -34,7 +54,6 @@ class App extends React.Component {
 
   selectNote = (note, index) =>
     this.setState({ selectedNoteIndex: index, selectedNote: note });
-
   noteUpdate = (id, noteObj) => {
     firebase
       .firestore()
@@ -46,18 +65,20 @@ class App extends React.Component {
         timestamp: firebase.firestore.FieldValue.serverTimestamp()
       });
   };
-
   newNote = async title => {
     const note = {
       title: title,
       body: ""
     };
-    const newFormDB = await firebase.firestore.collection("notes").add({
-      title: note.title,
-      body: note.body,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    });
-    const newID = newFormDB.id;
+    const newFromDB = await firebase
+      .firestore()
+      .collection("notes")
+      .add({
+        title: note.title,
+        body: note.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+    const newID = newFromDB.id;
     await this.setState({ notes: [...this.state.notes, note] });
     const newNoteIndex = this.state.notes.indexOf(
       this.state.notes.filter(_note => _note.id === newID)[0]
@@ -67,7 +88,6 @@ class App extends React.Component {
       selectedNoteIndex: newNoteIndex
     });
   };
-
   deleteNote = async note => {
     const noteIndex = this.state.notes.indexOf(note);
     await this.setState({
@@ -90,28 +110,6 @@ class App extends React.Component {
       .doc(note.id)
       .delete();
   };
-
-  render() {
-    return (
-      <div className="app-container">
-        <SidebarComponent
-          selectedNoteIndex={this.state.selectedNoteIndex}
-          notes={this.state.notes}
-          deleteNote={this.deleteNote}
-          selectNote={this.selectNote}
-          newNote={this.newNote}
-        />
-        {this.state.selectedNote ? (
-          <EditorComponent
-            selectedNote={this.state.selectedNote}
-            selectedNoteIndex={this.state.selectedNoteIndex}
-            notes={this.state.notes}
-            noteUpdate={this.noteUpdate}
-          />
-        ) : null}
-      </div>
-    );
-  }
 }
 
 export default App;
--- src/sidebaritem/sidebaritem.js
+++ src/sidebaritem/sidebaritem.js
@@ -7,8 +7,39 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import { removeHTMLTags } from "../helper";
 
 class SidebarItemComponent extends React.Component {
+  selectNote = (n, i) => this.props.selectNote(n, i);
+  deleteNote = note => {
+    if (window.confirm(`Are you sure you want to delete: ${note.title}`)) {
+      this.props.deleteNote(note);
+    }
+  };
+
   render() {
-    return <div> Welcome to sidebar items</div>;
+    const { _index, _note, classes, selectedNoteIndex } = this.props;
+
+    return (
+      <div key={_index}>
+        <ListItem
+          className={classes.listItem}
+          selected={selectedNoteIndex === _index}
+          alignItems="flex-start"
+        >
+          <div
+            className={classes.textSection}
+            onClick={() => this.selectNote(_note, _index)}
+          >
+            <ListItemText
+              primary={_note.title}
+              secondary={removeHTMLTags(_note.body.substring(0, 30)) + "..."}
+            ></ListItemText>
+          </div>
+          <DeleteIcon
+            onClick={() => this.deleteNote(_note)}
+            className={classes.deleteIcon}
+          ></DeleteIcon>
+        </ListItem>
+      </div>
+    );
   }
 }
 
