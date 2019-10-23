262ca35927d33c6e3f282e15cf2d41e2aa591936
Breyanna Vass <breyannavass@gmail.com>
2019-10-23 12:15:08 -0400

    renaming the page and elements for html page

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ firebase.initializeApp({
   measurementId: MEASUREMENT
 });
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("evernote-container"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
